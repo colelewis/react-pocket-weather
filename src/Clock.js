8b8945f842664095331476d694556357543b9f97
Cole Lewis <cole.g.lewis@gmail.com>
2022-05-11 22:44:29 -0400

    Clean up. More work pending planning on how to arrang elements.

---
+++ src/Clock.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+function Clock() {
+    const [now, setTime] = useState(new Date()); //use current date when triggering an update
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+        return () => {
+            clearInterval(timer);
+        }
+    }, []);
+    return now.toLocaleString();
+};
+
+export default Clock;
+
+
--- src/Weather.js
+++ src/Weather.js
@@ -1,32 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import useGeolocation from 'react-hook-geolocation';
+import Clock from './Clock';
 import './bootstrap.min.css';
 
 function Weather() {
 
-  const geolocation = useGeolocation();
+  // add icons eventually with this: https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
   const API_KEY = process.env.REACT_APP_API_KEY;
-  // const API_URL = 'https://api.openweathermap.org/data/2.5/weather?lat='+geolocation.latitude.toFixed(13)+'&lon='+geolocation.longitude+'&appid='+API_KEY;
 
-  // const DataFetch = () => { // functional
-  //   const [data, setData] = useState(''); // blank JSON information pre-API call
-  //   useEffect(() => {
-  //     fetch(API_URL, {"method": "GET"})
-  //     .then(fetchedData => fetchedData.json())
-  //     .then(response => {
-  //       setData(response)
-  //     });
-  //   }, []);
-  //   console.log(data);
-  //   // return data;
-  // }
+  function GeolocationDataFetch(lat, long) {
+    const API_URL = 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid='+API_KEY;
+    const [geoData, setGeoData] = useState();
+    useEffect(() => {
+      fetch(API_URL, {"method": "GET"})
+      .then(fetchedData => fetchedData.json())
+      .then(response => {
+        setGeoData(response)
+      });
+    }, []);
+    console.log(geoData);
+    return geoData;
+  } // gotta use some sort of callback with geolocation api
 
+  function ManualDataFetch(zip, country) {
+    const MANUAL_API_URL = 'https://api.openweathermap.org/data/2.5/weather?zip=' + zip + ',' + country + '&appid=' + API_KEY;
+    const [manualData, setManualData] = useState();
+    useEffect(() => {
+      fetch(MANUAL_API_URL, { "method": "GET" })
+        .then(fetchedData => fetchedData.json())
+        .then(response => {
+          setManualData(response)
+        });
+    }, []);
+    console.log(manualData);
+    // return manualData;
+  }
 
+  function convertUnix(utime) {
+    return new Date(utime * 1000).toLocaleString();
+  }
 
   return (
     <>
-      <p class="lead">test</p>
-      {/* {DataFetch()} */}
+      <div>
+        <Clock />
+      </div>
     </>
   );
 }
