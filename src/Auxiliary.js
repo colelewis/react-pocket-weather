22f6899a28791997205c528c8e1e5fe0f494ba4f
Cole Lewis <cole.g.lewis@gmail.com>
2022-05-19 20:22:23 -0400

    Element color changes based on time of day.

--- src/Auxiliary.js
+++ src/Auxiliary.js
@@ -25,5 +25,45 @@ export function convertUnix(utime) {
 }
 
 export function sunlightColorTransition(now) {
+    switch (now) {
+        case 0:
+        case 1:
+        case 2:
+            return '#1e2b58';
 
+        case 3:
+        case 4:
+        case 5:
+            return '#213166';
+
+        case 6:
+        case 7:
+        case 8:
+            return '#fffcb1';
+
+        case 9:
+        case 10:
+        case 11:
+            return '#f2e198';
+
+        case 12:
+        case 13:
+        case 14:
+            return '#e7cb23';
+
+        case 15:
+        case 16:
+        case 17:
+            return '#ffcb2a';
+
+        case 18:
+        case 19:
+        case 20:
+            return '#e7a165';
+
+        case 21:
+        case 22:
+        case 23:
+            return '#513869';
+    }
 }
--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,7 @@ import Clock from './components/Clock';
 import Dashboard from './components/Dashboard';
 import './bootstrap.min.css';
 import ManualDashboard from './components/ManualDashboard';
+import { sunlightColorTransition } from './Auxiliary';
 
 export default class Weather extends Component {
   constructor(props) {
@@ -86,11 +87,13 @@ export default class Weather extends Component {
   }
 
   render() {
-    const COLOR_CODE = '#4e73ad'; // to eventually be provided by a function to provide a color from the time
+    // const COLOR_CODE = '#4e73ad';
+    const COLOR_CODE = sunlightColorTransition(new Date().getHours());
+    // console.log(sunlightColorTransition(20));
     return (
       <>
         <div className='container-fluid bg-light mb-0 p-2'>
-          <h2>react-pocket-weather</h2>
+          <h2>react-pocket-weather <a href="https://github.com/colelewis/react-pocket-weather"><svg style={{float: 'right'}} xmlns="http://www.w3.org/2000/svg" width="5%" height="5%" fill={COLOR_CODE} className="bi bi-github m-1" viewBox="0 0 16 16"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/></svg></a></h2>
           <p className='lead fs-4 text-wrap'>Your current date and time are: <Clock /></p>
           {this.UnitSelector()}
         </div>
@@ -105,10 +108,6 @@ export default class Weather extends Component {
           <div className='col p-3' style={{ backgroundColor: COLOR_CODE }}>
             {this.ManualDashboardWrapper()}
           </div>
-        </div>
-
-        <div className='container-fluid mx-auto fixed-bottom p-2 mt-3' style={{ backgroundColor: COLOR_CODE }}>
-          <a href="https://github.com/colelewis/react-pocket-weather"><svg xmlns="http://www.w3.org/2000/svg" width="7%" height="7%" fill="white" className="bi bi-github mx-auto d-block" viewBox="0 0 16 16"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/></svg></a>
         </div>
       </>
     );
